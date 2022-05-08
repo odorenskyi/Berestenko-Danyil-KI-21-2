0719e3213c6dec9b4e595f4a535edaadcda227cb
Daniil Berestenko <berestenkodaniilki212@gmail.com>
2022-05-08 20:52:48 +0300

    Added static library for lab 10

---
+++ lab10/prj/ModulesBerestenko/Modules-berestenko.h
@@ -0,0 +1,10 @@
+#ifndef MODULES-BERESTENKO_H_INCLUDED
+#define MODULES-BERESTENKO_H_INCLUDED
+
+using namespace std;
+
+float s_calculation(float x, float y,float z);
+void inputFileTxt(string inputFilename,string sentence);
+void authorInfo(string outputFileName);
+
+#endif // MODULES-BERESTENKO_H_INCLUDED
